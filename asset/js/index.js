ae7156b45f731c199e1d794c26baff4b52916fac
Austin <austinhuffman0@gmail.com>
2021-06-25 15:53:49 -0400

    Color Functionality Works and Storage almost complete

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,5 +1,4 @@
 var now = moment().format("dddd, MMMM Do YYYY, h:mm a"); 
-var hour = moment().format("k");
 
 //SETS HEADER TO CURRENT TIME
 function headerTime(){
@@ -7,34 +6,33 @@ var header = document.getElementById("currentDay");
 header.textContent = now;
 }
 
-
-
 function populateHours(){
-    let timeArr = [];
-    let timeList = $('.time');
+    let timeList = $('textarea');
+    var hour = moment().format("k");
 
+    //loops through every element that is a textarea and uses a nested ternary operator to assign colors of past present and future
     timeList.each(function(i){
-        timeArr.push(this);
-    });
-
-    // let hour = moment(timeArr[1].textContent).format("kk a");
-    // console.log(hour);
-    // if(hour<now)
-    // console.log("EURIKA");
-
-    if ( 8 < hour){
-        console.log("You are an idiot");
-    }
-    //if(timeArr[1].isBefore(now))
-   // console.log(timeArr[0].textContent); // use this to grab each links time
-
+        let listHour = this.dataset.hour;
+        listHour == hour ? $(this).addClass('present') : listHour > hour ? $(this).addClass('future') : $(this).addClass('past');
+     });
 }
 
-//check KK time
+
+
+let storage = window.localStorage;
+var saveButton = $('.saveBtn');
+
+saveButton.on('click', function() {
+    let targetField =  $(this).siblings('textarea');
+    storage.setItem(targetField.data('hour'), JSON.stringify(targetField.val()));
+    console.log(targetField.data('hour'));
+});
+
+
 function init(){
     headerTime();
     populateHours();
+    console.log(JSON.parse(storage.getItem('08')));
 }
 
 init();
-
